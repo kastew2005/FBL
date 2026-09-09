692589aa737b7d90ea0926e45b8780dc837525d5
kastew2005 <kastew2911@icloud.com>
2026-09-09 22:51:30 +0300

    Create mobs.js

---
+++ mobs.js
@@ -0,0 +1,20 @@
+class Villager {
+    constructor(scene, x, y, z) {
+        this.group = new THREE.Group();
+        
+        const bodyGeo = new THREE.BoxGeometry(0.6, 1.2, 0.5);
+        const bodyMat = new THREE.MeshStandardMaterial({ color: 0x5c3a21 });
+        const body = new THREE.Mesh(bodyGeo, bodyMat);
+        body.position.y = 0.6;
+        this.group.add(body);
+
+        const headGeo = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+        const headMat = new THREE.MeshStandardMaterial({ color: 0x966f43 });
+        const head = new THREE.Mesh(headGeo, headMat);
+        head.position.y = 1.45;
+        this.group.add(head);
+
+        this.group.position.set(x, y, z);
+        scene.add(this.group);
+    }
+}
