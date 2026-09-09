4cfee4efe6d38ba294dd601653bc6392a31f8d7b
kastew2005 <kastew2911@icloud.com>
2026-09-09 22:21:14 +0300

    Update player.js

--- player.js
+++ player.js
@@ -1,11 +1,10 @@
 class Player {
     constructor() {
-        this.x = 0; this.y = 6; this.z = 0;
+        this.x = 0; this.y = 8; this.z = 0;
         this.rotationX = 0; this.rotationY = 0;
-        this.speed = 0.09;
+        this.speed = 0.085;
         this.vy = 0;
         this.bobbingTimer = 0;
-        this.isMoving = false;
     }
 
     update(moveState, camera, handMesh) {
@@ -18,33 +17,27 @@ class Player {
         if (moveState.right) moveDir.add(sideVector);
         if (moveState.left) moveDir.sub(sideVector);
 
-        this.isMoving = moveDir.length() > 0;
-
-        if (this.isMoving) {
+        if (moveDir.length() > 0) {
             moveDir.normalize();
             this.x += moveDir.x * this.speed;
             this.z += moveDir.z * this.speed;
 
-            // ЭФФЕКТ ПОКАЧИВАНИЯ КАМЕРЫ (Head Bobbing)
-            this.bobbingTimer += 0.15;
-            if (Math.sin(this.bobbingTimer) < -0.9) sounds.playStep();
+            this.bobbingTimer += 0.16;
+            if (Math.sin(this.bobbingTimer) < -0.92) sounds.playStep();
         } else {
             this.bobbingTimer = 0;
         }
 
-        // Физика гравитации
         this.vy -= 0.007;
         this.y += this.vy;
         if (this.y < 2) { this.y = 2; this.vy = 0; }
 
-        // Позиция камеры с учетом эффекта ходьбы
-        let bobY = Math.sin(this.bobbingTimer) * 0.05;
-        let bobX = Math.cos(this.bobbingTimer * 0.5) * 0.03;
+        let bobY = Math.sin(this.bobbingTimer) * 0.04;
+        let bobX = Math.cos(this.bobbingTimer * 0.5) * 0.02;
 
-        camera.position.set(this.x + bobX, this.y + 1.6 + bobY, this.z);
+        camera.position.set(this.x + bobX, this.y + 1.5 + bobY, this.z);
         camera.quaternion.setFromEuler(new THREE.Euler(this.rotationX, this.rotationY, 0, 'YXZ'));
 
-        // Анимация 3D руки/инструмента
         if (handMesh) {
             handMesh.position.set(0.35 + bobX, -0.35 + bobY, -0.5);
         }
