641bea479a2c740176987feba99da2618b8fe1cf
kastew2005 <kastew2911@icloud.com>
2026-09-09 23:00:18 +0300

    Update player.js

--- player.js
+++ player.js
@@ -7,8 +7,24 @@ class Player {
         this.height = 1.8;
         this.heightOffset = 1.6;
         this.isGrounded = false;
-        this.speed = 0.12;
+        
+        this.baseSpeed = 0.12;
+        this.speed = this.baseSpeed;
         this.auraActive = false;
+    }
+
+    // Включение / Выключение Ауры Дракона
+    toggleAura() {
+        this.auraActive = !this.auraActive;
+        const auraStatusUI = document.getElementById('aura-status');
+
+        if (this.auraActive) {
+            this.speed = this.baseSpeed * 1.85; // Бафф скорости
+            if (auraStatusUI) auraStatusUI.style.display = 'block';
+        } else {
+            this.speed = this.baseSpeed;
+            if (auraStatusUI) auraStatusUI.style.display = 'none';
+        }
     }
 
     getAABB() {
