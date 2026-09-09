f87249bd9dcdb452d72449cbc40b10fbde6acc30
kastew2005 <kastew2911@icloud.com>
2026-09-09 22:59:57 +0300

    Update effects.js

--- effects.js
+++ effects.js
@@ -3,14 +3,23 @@ class VisualFX {
         this.scene = scene;
     }
 
-    spawnParticles(x, y, z) {
-        const pGeo = new THREE.BoxGeometry(0.1, 0.1, 0.1);
-        const pMat = new THREE.MeshBasicMaterial({ color: 0xffaa00 });
-        for (let i = 0; i < 8; i++) {
+    // Частицы Ауры Дракона [F]
+    spawnAuraParticles(playerPosition) {
+        const pGeo = new THREE.BoxGeometry(0.08, 0.08, 0.08);
+        const pMat = new THREE.MeshBasicMaterial({ color: 0x8a2be2 });
+        
+        for (let i = 0; i < 2; i++) {
             const p = new THREE.Mesh(pGeo, pMat);
-            p.position.set(x, y, z);
+            p.position.set(
+                playerPosition.x + (Math.random() - 0.5) * 1.4,
+                playerPosition.y - 0.4 + Math.random(),
+                playerPosition.z + (Math.random() - 0.5) * 1.4
+            );
             this.scene.add(p);
-            setTimeout(() => this.scene.remove(p), 1000);
+            
+            setTimeout(() => {
+                this.scene.remove(p);
+            }, 500);
         }
     }
 }
