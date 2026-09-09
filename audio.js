47a99617a8ac53aa903e1c028b359508f8f562b1
kastew2005 <kastew2911@icloud.com>
2026-09-09 22:19:57 +0300

    Update audio.js

--- audio.js
+++ audio.js
@@ -1,12 +1,12 @@
-// Звуки сгенерированы процедурно с помощью Web Audio API
 class SoundEngine {
-    constructor() {
-        this.ctx = null;
-    }
+    constructor() { this.ctx = null; }
 
     init() {
         if (!this.ctx) {
             this.ctx = new (window.AudioContext || window.webkitAudioContext)();
+        }
+        if (this.ctx.state === 'suspended') {
+            this.ctx.resume();
         }
     }
 
@@ -15,14 +15,14 @@ class SoundEngine {
         let osc = this.ctx.createOscillator();
         let gain = this.ctx.createGain();
         osc.type = 'triangle';
-        osc.frequency.setValueAtTime(80, this.ctx.currentTime);
-        osc.frequency.exponentialRampToValueAtTime(30, this.ctx.currentTime + 0.08);
-        gain.gain.setValueAtTime(0.15, this.ctx.currentTime);
-        gain.gain.linearRampToValueAtTime(0.01, this.ctx.currentTime + 0.08);
+        osc.frequency.setValueAtTime(70, this.ctx.currentTime);
+        osc.frequency.exponentialRampToValueAtTime(20, this.ctx.currentTime + 0.07);
+        gain.gain.setValueAtTime(0.1, this.ctx.currentTime);
+        gain.gain.linearRampToValueAtTime(0.01, this.ctx.currentTime + 0.07);
         osc.connect(gain);
         gain.connect(this.ctx.destination);
         osc.start();
-        osc.stop(this.ctx.currentTime + 0.08);
+        osc.stop(this.ctx.currentTime + 0.07);
     }
 
     playPlace() {
@@ -30,19 +30,19 @@ class SoundEngine {
         let osc = this.ctx.createOscillator();
         let gain = this.ctx.createGain();
         osc.type = 'sine';
-        osc.frequency.setValueAtTime(160, this.ctx.currentTime);
-        osc.frequency.exponentialRampToValueAtTime(240, this.ctx.currentTime + 0.06);
-        gain.gain.setValueAtTime(0.3, this.ctx.currentTime);
-        gain.gain.linearRampToValueAtTime(0.01, this.ctx.currentTime + 0.06);
+        osc.frequency.setValueAtTime(140, this.ctx.currentTime);
+        osc.frequency.exponentialRampToValueAtTime(220, this.ctx.currentTime + 0.05);
+        gain.gain.setValueAtTime(0.25, this.ctx.currentTime);
+        gain.gain.linearRampToValueAtTime(0.01, this.ctx.currentTime + 0.05);
         osc.connect(gain);
         gain.connect(this.ctx.destination);
         osc.start();
-        osc.stop(this.ctx.currentTime + 0.06);
+        osc.stop(this.ctx.currentTime + 0.05);
     }
 
     playBreak() {
         if (!this.ctx) return;
-        let bufferSize = this.ctx.sampleRate * 0.1;
+        let bufferSize = this.ctx.sampleRate * 0.08;
         let buffer = this.ctx.createBuffer(1, bufferSize, this.ctx.sampleRate);
         let data = buffer.getChannelData(0);
         for (let i = 0; i < bufferSize; i++) data[i] = Math.random() * 2 - 1;
@@ -51,7 +51,7 @@ class SoundEngine {
         noise.buffer = buffer;
         let gain = this.ctx.createGain();
         gain.gain.setValueAtTime(0.2, this.ctx.currentTime);
-        gain.gain.linearRampToValueAtTime(0.01, this.ctx.currentTime + 0.1);
+        gain.gain.linearRampToValueAtTime(0.01, this.ctx.currentTime + 0.08);
         noise.connect(gain);
         gain.connect(this.ctx.destination);
         noise.start();
@@ -62,14 +62,14 @@ class SoundEngine {
         let osc = this.ctx.createOscillator();
         let gain = this.ctx.createGain();
         osc.type = 'sine';
-        osc.frequency.setValueAtTime(150, this.ctx.currentTime);
-        osc.frequency.exponentialRampToValueAtTime(300, this.ctx.currentTime + 0.12);
-        gain.gain.setValueAtTime(0.2, this.ctx.currentTime);
-        gain.gain.linearRampToValueAtTime(0.01, this.ctx.currentTime + 0.12);
+        osc.frequency.setValueAtTime(130, this.ctx.currentTime);
+        osc.frequency.exponentialRampToValueAtTime(260, this.ctx.currentTime + 0.1);
+        gain.gain.setValueAtTime(0.15, this.ctx.currentTime);
+        gain.gain.linearRampToValueAtTime(0.01, this.ctx.currentTime + 0.1);
         osc.connect(gain);
         gain.connect(this.ctx.destination);
         osc.start();
-        osc.stop(this.ctx.currentTime + 0.12);
+        osc.stop(this.ctx.currentTime + 0.1);
     }
 }
 
